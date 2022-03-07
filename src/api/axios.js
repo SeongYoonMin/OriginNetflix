664c074fd80abbdda34f79b7c92d9d3f4ac92e45
Villain_uni <namo4862@naver.com>
2022-03-07 17:51:37 +0900

    [ADD] axios,requset 설정 및 Components 추가

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: "",
+    api_key: "05c11592e49074e0fc9e58600c57c842",
     language: "ko-KR",
   },
 })
